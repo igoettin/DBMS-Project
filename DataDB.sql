eb58bc010dcc5c911f0d9dc70adfc6fbcf7cd70b
Ian Goetting <igoettin@thompson.cs.nmsu.edu>
2017-09-21 15:42:41 -0600

    Modified insertions to use varchar strings not ints

--- DataDB.sql
+++ DataDB.sql
@@ -1,35 +1,35 @@
 insert into Player(ID, LoginID, Name, Password, Birthday, Address, Email, PhoneNumber, PlayPos)
-values(1010, 220, 'Sebastian', 'pass', '1993-12-01', 'Garcia', 'sebastian44@nmsu.edu', 911, 'shooting guard');
+values(1010, '220', 'Sebastian', 'pass', '1993-12-01', 'Garcia', 'sebastian44@nmsu.edu', '1214356911', 'shooting guard');
 insert into Player(ID, LoginID, Name, Password, Birthday, Address, Email, PhoneNumber, PlayPos)
-values(1011, 221, 'Thomas', 'pass1', '1991-11-11', 'Orange Street', 'tom192@nmsu.edu', 9105112345, 'small forward');
+values(1011, '221', 'Thomas', 'pass1', '1991-11-11', 'Orange Street', 'tom192@nmsu.edu', '9105112345', 'small forward');
 insert into Player(ID, LoginID, Name, Password, Birthday, Address, Email, PhoneNumber, PlayPos)
-values(1012, 222, 'Jon', 'pass2', '1993-08-04', 'Blue Street', 'jon192781@nmsu.edu', 9102546577, 'point guard');
+values(1012, '222', 'Jon', 'pass2', '1993-08-04', 'Blue Street', 'jon192781@nmsu.edu', '9102546577', 'point guard');
 insert into Player(ID, LoginID, Name, Password, Birthday, Address, Email, PhoneNumber, PlayPos)
-values(1013, 223, 'Bob', 'pass3', '1993-01-14', 'Green Ave', 'bob2121@nmsu.edu', 7834758676, 'power forward');
+values(1013, '223', 'Bob', 'pass3', '1993-01-14', 'Green Ave', 'bob2121@nmsu.edu', '7834758676', 'power forward');
 insert into Player(ID, LoginID, Name, Password, Birthday, Address, Email, PhoneNumber, PlayPos)
-values(1014, 224, 'Derek', 'pass4', '1983-03-01', 'Red Street', 'derek2982@nmsu.edu', 8793921011, 'center');
+values(1014, '224', 'Derek', 'pass4', '1983-03-01', 'Red Street', 'derek2982@nmsu.edu', '8793921011', 'center');
 
 insert into Manager(ID,LoginID,Name,Password,Birthday,Address,Email,PhoneNumber)
-values(10, 20, 'Ryan', 'passw', '1994-11-05', 'Farm',  'ryan121@nmsu.edu', 4112345467);
+values(10, '20', 'Ryan', 'passw', '1994-11-05', 'Farm',  'ryan121@nmsu.edu', '4112345467');
 insert into Manager(ID,LoginID,Name,Password,Birthday,Address,Email,PhoneNumber)
-values(11, 21, 'Suzan', 'passw1', '1992-10-05', 'City',  'suzan981@nmsu.edu', 4232345467);
+values(11, '21', 'Suzan', 'passw1', '1992-10-05', 'City',  'suzan981@nmsu.edu', '4232345467');
 insert into Manager(ID,LoginID,Name,Password,Birthday,Address,Email,PhoneNumber)
-values(12, 22, 'Bill', 'passw2', '1991-09-01', 'Country',  'bill25@nmsu.edu', 7112115467);
+values(12, '22', 'Bill', 'passw2', '1991-09-01', 'Country',  'bill25@nmsu.edu', '7112115467');
 insert into Manager(ID,LoginID,Name,Password,Birthday,Address,Email,PhoneNumber)
-values(13, 23, 'Zoey', 'passw3', '1984-04-02', 'City',  'zoey123@nmsu.edu', 4112345467);
+values(13, '23', 'Zoey', 'passw3', '1984-04-02', 'City',  'zoey123@nmsu.edu', '4112345467');
 insert into Manager(ID,LoginID,Name,Password,Birthday,Address,Email,PhoneNumber)
-values(14, 24, 'Louis', 'passw4', '1968-10-05', 'Place',  'louis121@nmsu.edu', 4112345467);
+values(14, '24', 'Louis', 'passw4', '1968-10-05', 'Place',  'louis121@nmsu.edu', '4112345467');
 
 insert into Staff(ID,Name,Birthday,Address,Email,PhoneNumber,Title)
-values(30, 'Chris', '1994-01-09', 'Maine', 'chris21@gmail.com', 1018796758, 'Treasurer');
+values(30, 'Chris', '1994-01-09', 'Maine', 'chris21@gmail.com', '1018796758', 'Treasurer');
 insert into Staff(ID,Name,Birthday,Address,Email,PhoneNumber,Title)
-values(31, 'Debby', '1991-02-09', 'Florida', 'deb21@gmail.com', 1018349675, 'Secretary');
+values(31, 'Debby', '1991-02-09', 'Florida', 'deb21@gmail.com', '1018349675', 'Secretary');
 insert into Staff(ID,Name,Birthday,Address,Email,PhoneNumber,Title)
-values(32, 'Max', '1991-03-09', 'Texas', 'max212@gmail.com', 1018796751, 'Health Advisor');
+values(32, 'Max', '1991-03-09', 'Texas', 'max212@gmail.com', '1018796751', 'Health Advisor');
 insert into Staff(ID,Name,Birthday,Address,Email,PhoneNumber,Title)
-values(33, 'Ashley', '1990-01-02', 'New Mexico', 'ashle21@gmail.com', 1018726758, 'Water Girl');
+values(33, 'Ashley', '1990-01-02', 'New Mexico', 'ashle21@gmail.com', '1018726758', 'Water Girl');
 insert into Staff(ID,Name,Birthday,Address,Email,PhoneNumber,Title)
-values(34, 'Zack', '1984-02-09', 'North Carolina', 'zacl21@gmail.com', 1018791758, 'Coach');
+values(34, 'Zack', '1984-02-09', 'North Carolina', 'zacl21@gmail.com', '1018791758', 'Coach');
 
 
 insert into ManagerCertificate(ManagerID,CertificateID,Certificate)
@@ -44,15 +44,15 @@ insert into ManagerCertificate(ManagerID,CertificateID,Certificate)
 values(14,44,'a');
 
 insert into Stats(PlayerID,Year,TotalPoints,ASPG)
-values(1010,2017,10,15);
+values(1010,'2017',10,15);
 insert into Stats(PlayerID,Year,TotalPoints,ASPG)
-values(1011,2017,20,30);
+values(1011,'1998',20,30);
 insert into Stats(PlayerID,Year,TotalPoints,ASPG)
-values(1012,2017,30,45);
+values(1012,'1999',30,45);
 insert into Stats(PlayerID,Year,TotalPoints,ASPG)
-values(1013,2017,50,60);
+values(1013,'2005',50,60);
 insert into Stats(PlayerID,Year,TotalPoints,ASPG)
-values(1014,2017,60,75);
+values(1014,'2017',60,75);
 
 insert into Training(TrainingName, Instruction,TimePeriodInHour)
 values('Running','Run 20 miles around the circuit', 8);
