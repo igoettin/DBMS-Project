f1eeaaffda5c4bfe0c1d66b3f391d5830592dc04
Ian Goetting <igoettin@backus.cs.nmsu.edu>
2017-11-04 20:15:47 -0600

    fix error on download certificate

--- cs482502/config.php
+++ cs482502/config.php
@@ -1,10 +1,7 @@
-<!-- Configuration PHP file for connecting to the server and linking the page up with the database -->
 <?php
-   //Connect to the database server;
    $con = mysql_connect('dbclass.cs.nmsu.edu','igoettin','v0PTzY92');
    if(!$con) {
     die('Could not connect: ' . mysql_error());
    }
-   //Select the cs482502fa17_igoettin as the database we want to use
    mysql_select_db("cs482502fa17_igoettin", $con);
 ?>
